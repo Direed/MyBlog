937d4b1be73aef59486b7a61c893dc2abd814450
Direed <toxa.vikhrest@mail.ru>
2021-01-06 22:42:34 +0200

    Global change(JS mechanics and background color and other..)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// Progress bar for my skills //
 const masSkills = [
     {name: "js", progressSkill: 65},
     {name: "css", progressSkill: 60},
@@ -40,9 +41,98 @@ const loadSkills = () => {
     })
 }
 window.addEventListener("load", loadSkills);
+// Progress bar for my skills //
+
+// Open contacts in social networking //
+const myContacts = document.getElementsByClassName('contacts');
+for (let contact of myContacts) {
+    contact.addEventListener('click', () => {
+        switch (contact.id) {
+            case "telegram-contact":
+                window.open("https://t.me/PrettyWigga");
+                break;
+            case "facebook-contact":
+                window.open("https://www.facebook.com/anton.vikhrest/")
+                break;
+            case "linkedIn-contact":
+                window.open("https://www.linkedin.com/in/anton-vyhrest-8709041bb/")
+            default:
+                console.log("no posts");
+                break;
+        }
+    })
+};
+// Open contacts in social networking //
+
+// Open block on page //
+/*const myBlock = document.getElementsByClassName('li-block');
+for (let block of myBlock){
+    block.addEventListener('click', () => {
+        switch (block.id) {
+            case "li-main":
+                location.href = '#mainBox';
+                break;
+            case "li-my-skills":
+                location.href = '#mySkills';
+                break;
+            case "li-portfolio":
+                location.href = '#myPortfolio';
+                break;
+            case "linkedIn-contact":
+                location.href = 'mainBox';
+                break;
+            case "linkedIn-contact":
+                location.href = 'mainBox';
+                break;
+            default:
+                console.log("no posts");
+                break;
+        }
+    })
+}
+// Open block on page //
+*/
+
+
+// Open block on page //
+const myLiBlock = document.getElementsByClassName('li-block');
+const myBlock = document.getElementsByClassName('block');
+for (let block of myLiBlock){
+    block.addEventListener('click', () => {
+        switch (block.id) {
+            case "li-main":
+                for (let block_hidden of myBlock) {
+                    if(block_hidden.id != "mainBox") block_hidden.style.display = "none";
+                    else block_hidden.style.display = "block";
+                }
+                break;
+            case "li-my-skills":
+                for (let block_hidden of myBlock) {
+                    if(block_hidden.id != "mySkills") block_hidden.style.display = "none";
+                    else block_hidden.style.display = "block";
+                }
+                break;
+            case "li-portfolio":
+                for (let block_hidden of myBlock) {
+                    if(block_hidden.id != "myPortfolio") block_hidden.style.display = "none";
+                    else block_hidden.style.display = "grid";
+                }
+                break;
+            case "linkedIn-contact":
+                block.style.display = block;
+                break;
+            case "linkedIn-contact":
+                block.style.display = block;
+                break;
+            default:
+                console.log("no posts");
+                break;
+        }
+    })
+}
+// Open block on page //
 
 const load = document.getElementById("loadForm");
-
 
 const rectangles = document.getElementsByClassName('loadRectangle');
 anime({
